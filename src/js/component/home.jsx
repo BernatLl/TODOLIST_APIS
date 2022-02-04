31492770f16107dd6e55941b59fdcc84a1c5ca17
BernatLl <blloret@hotmail.com>
2022-02-04 13:45:32 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TareaForm from "./TareaForm.jsx";
 import "../../styles/index.css";
 import Tarea from "./Tarea.jsx";
 
 const Home = () => {
+	/*const task = {
+		label: "",
+		done: false,
+	}; */
 	const [listaTareas, setListaTareas] = useState([]);
 
 	const nuevaTarea = (tarea) => {
 		setListaTareas([tarea, ...listaTareas]);
 	};
+
 	const borrar = (id) => {
 		const listaFiltrada = listaTareas.filter((e, index) => index !== id);
 		setListaTareas(listaFiltrada);
 	};
 
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/BernatLl", {
+			method: "GET",
+			body: JSON.stringify(todos),
+			headers: {
+				Accept: "application/json",
+				"Content-type": "application/json",
+			},
+		})
+			.then((resp) => {
+				return resp.json();
+			})
+			.then((data) => {
+				setListaTareas(data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []);
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/BernatLl", {
+			method: "PUT",
+			body: JSON.stringify(todos),
+			headers: {
+				Accept: "application/json",
+				"Content-type": "application/json",
+			},
+		})
+			.then((resp) => {
+				return resp.json();
+			})
+			.then((data) => {
+				nuevaTarea(data);
+				console.log(data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	});
 	return (
 		<div className="container">
 			<TareaForm nuevaTarea={nuevaTarea} />
